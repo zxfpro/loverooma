a6b99b6f450186b5cbc40fce72b77b76ba86dedc
823042332@qq.com <823042332@qq.com>
2025-07-30 13:50:09 +0800

    2025年07月30日13:50:08

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from loverooma.connect_kb import work
+
+wk = work()
+
+wk.update('hello3sdf32vv',id='2cffg344')
+
+wk.create_collection(vector_dimension=2560)
+
+wk.reload()
+
+wk.search('hellocc')
+
+wk.update('hello332vv',id='ddcccd')
+
+wk.index.delete(doc_id = "222333")
